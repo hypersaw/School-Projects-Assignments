562a486834db7198846820d8a886964fd7ea0675
Andrew Long <andrew@isandrewlong.com>
2013-01-25 13:45:29 -0600

    Added CS440 folder and initial Game Prototype #1 project.

---
+++ Fall 2012 - CS474/Abstract Dog/Abstract Dog/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Abstract Dog
+//
+//  Created by Andrew Long on 12/12/12.
+//  Copyright (c) 2012 Andrew Long. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
---
+++ Spring 2013 - CS440/DescriptiveIsland/DescriptiveIsland/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DescriptiveIsland
+//
+//  Created by Andrew Long on 1/25/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
