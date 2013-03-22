219ffc684d89e9c84bc8f2ced143bb673ebc785d
Andrew Long <along27@uic.edu>
2013-03-22 01:17:44 -0500

    Added rules and methods for them.

--- Spring 2013 - CS301/Recursive Decent Parsing/Recursive Decent Parsing/REParser.cpp
+++ Spring 2013 - CS301/Recursive Decent Parsing/Recursive Decent Parsing/REParser.cpp
@@ -1,11 +1,25 @@
 //
-//  REParser.cpp
+//  REParser.h
 //  Regular Expression Parser
 //
 //  Andrew Long
 //  University of Illinois at Chicago
 //  CS301, Spring 2013: HW8-PE
 //
+//  GRAMMAR:
+//  1.  <RE> 		-> 	<CONCAT><RE’>
+//  2.  <RE’>		->	U<CONCAT>
+//  3.  <CONCAT> 	->	<KLEEN><CONCAT’>
+//  4.  <CONCAT’>	->	<KLEEN><CONCAT’>
+//  5.  <CONCAT’>	->	/e/
+//  6.  <KLEEN>     ->	<ELEMENT><KLEEN’>
+//  7.  <KLEEN’>	->	*<KLEEN’>
+//  8.  <KLEEN’>	->	/e/
+//  9.  <ELEMENT>	->	a
+//  10. <ELEMENT>	->	b
+//  11. <ELEMENT>	->	c
+//  12. <ELEMENT>	->	d
+//  13. <ELEMENT>	->	(<RE>)
 
 #include "REParser.h"
 
@@ -17,19 +31,63 @@ REParser::~REParser(){
     
 }
 
-void REParser::parse(char* input){
-    userInput = input;
+void REParser::parse(std::string input){
+    userInput = input+'$';
     index = 0;
+    
+    try{
+        RE();
+        match('$');
+        
+        std::cout << std::endl;
+        std::cout << "** Yes, input is a valid RE!" << std::endl;
+        std::cout << std::endl;
+        
+    }
+    catch(...){
+        std::cout << std::endl;
+        std::cout << "** Sorry, input is NOT a valid RE..." << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 char REParser::nextCharacter(){    
     return userInput[index];
 }
 
-void REParser::match(char* symbol){
+void REParser::match(char symbol){
     
 }
 
 void REParser::consume(){
     ++index;
 }
+
+void REParser::RE(){
+    
+}
+
+void REParser::_RE(){
+    
+}
+
+void REParser::CONCAT(){
+    
+}
+
+void REParser::_CONCAT(){
+    
+}
+
+void REParser::KLEENE(){
+    
+}
+
+void REParser::_KLEENE(){
+    
+}
+
+void REParser::ELEMENT(){
+    
+}
+
--- Spring 2013 - CS301/Recursive Decent Parsing/Recursive Decent Parsing/REParser.h
+++ Spring 2013 - CS301/Recursive Decent Parsing/Recursive Decent Parsing/REParser.h
@@ -6,25 +6,49 @@
 //  University of Illinois at Chicago
 //  CS301, Spring 2013: HW8-PE
 //
+//  GRAMMAR:
+//  1.  <RE> 		-> 	<CONCAT><RE’>
+//  2.  <RE’>		->	U<CONCAT>
+//  3.  <CONCAT> 	->	<KLEEN><CONCAT’>
+//  4.  <CONCAT’>	->	<KLEEN><CONCAT’>
+//  5.  <CONCAT’>	->	/e/
+//  6.  <KLEEN>     ->	<ELEMENT><KLEEN’>
+//  7.  <KLEEN’>	->	*<KLEEN’>
+//  8.  <KLEEN’>	->	/e/
+//  9.  <ELEMENT>	->	a
+//  10. <ELEMENT>	->	b
+//  11. <ELEMENT>	->	c
+//  12. <ELEMENT>	->	d
+//  13. <ELEMENT>	->	(<RE>)
+
 
 #ifndef REPARSER
 #define REPARSER
 
 #include <iostream>
+#include <string>
 
 class REParser{
 public:
     REParser();
     ~REParser();
     
-    void parse(char* input);
+    void parse(std::string input);
 private:
     unsigned int index;
-    char* userInput;
+    std::string userInput;
     
     char nextCharacter();
-    void match(char* symbol);
+    void match(char symbol);
     void consume();
+    
+    void RE();
+    void _RE();
+    void CONCAT();
+    void _CONCAT();
+    void KLEENE();
+    void _KLEENE();
+    void ELEMENT();
 protected:
     
 };
